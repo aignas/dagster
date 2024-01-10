2ecd06feb3ec45dff65e6ad78292ae0dffd27a6c
benpankow <ben@dagsterlabs.com>
2024-01-09 16:27:13 -0800

    [ui] allow meta+click to select all assets in group

--- js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetGraphExplorer.tsx
+++ js_modules/dagster-ui/packages/ui-core/src/asset-graph/AssetGraphExplorer.tsx
@@ -383,13 +383,41 @@ const AssetGraphExplorerWithData = ({
     selectNodeById(e, nextId);
   };
 
+  const selectAllGroupNodesById = React.useCallback(
+    (e: React.MouseEvent<any> | React.KeyboardEvent<any>, groupId: string) => {
+      const assets = groupedAssets[groupId] || [];
+      const childNodeTokens = assets.map((n) => tokenForAssetKey(n.assetKey));
+
+      const existing = explorerPath.opNames[0]!.split(',');
+
+      const nextOpsNameSelection = childNodeTokens.every((token) => existing.includes(token))
+        ? uniq(without(existing, ...childNodeTokens)).join(',')
+        : uniq([...existing, ...childNodeTokens]).join(',');
+
+      onChangeExplorerPath(
+        {
+          ...explorerPath,
+          opNames: [nextOpsNameSelection],
+        },
+        'replace',
+      );
+    },
+    [groupedAssets, explorerPath, onChangeExplorerPath],
+  );
+
   const selectNodeById = React.useCallback(
     (e: React.MouseEvent<any> | React.KeyboardEvent<any>, nodeId?: string) => {
       if (!nodeId) {
         return;
       }
-      if (isGroupId(nodeId)) {
-        zoomToGroup(nodeId);
+      const modifiedNodeId = '__repository__@' + nodeId;
+      if (isGroupId(modifiedNodeId)) {
+        zoomToGroup(modifiedNodeId);
+
+        if (e.metaKey) {
+          selectAllGroupNodesById(e, modifiedNodeId);
+        }
+
         return;
       }
       const node = assetGraphData.nodes[nodeId];
@@ -406,7 +434,15 @@ const AssetGraphExplorerWithData = ({
         setExpandedGroups([...expandedGroups, groupIdForNode(node)]);
       }
     },
-    [assetGraphData.nodes, layout, onSelectNode, zoomToGroup, expandedGroups, setExpandedGroups],
+    [
+      assetGraphData.nodes,
+      onSelectNode,
+      layout,
+      zoomToGroup,
+      selectAllGroupNodesById,
+      setExpandedGroups,
+      expandedGroups,
+    ],
   );
 
   const [showSidebar, setShowSidebar] = React.useState(isGlobalGraph);
@@ -493,9 +529,13 @@ const AssetGraphExplorerWithData = ({
                     assets: groupedAssets[group.id] || [],
                   }}
                   minimal={scale < MINIMAL_SCALE}
-                  onCollapse={() => {
-                    focusGroupIdAfterLayoutRef.current = group.id;
-                    setExpandedGroups(expandedGroups.filter((g) => g !== group.id));
+                  onCollapse={(e) => {
+                    if (e.metaKey) {
+                      selectAllGroupNodesById(e, group.id);
+                    } else {
+                      focusGroupIdAfterLayoutRef.current = group.id;
+                      setExpandedGroups(expandedGroups.filter((g) => g !== group.id));
+                    }
                   }}
                 />
               </foreignObject>
@@ -541,9 +581,13 @@ const AssetGraphExplorerWithData = ({
                     assetCount: allGroupCounts[group.id] || 0,
                     assets: groupedAssets[group.id] || [],
                   }}
-                  onExpand={() => {
-                    focusGroupIdAfterLayoutRef.current = group.id;
-                    setExpandedGroups([...expandedGroups, group.id]);
+                  onExpand={(e) => {
+                    if (e.metaKey) {
+                      selectAllGroupNodesById(e, group.id);
+                    } else {
+                      focusGroupIdAfterLayoutRef.current = group.id;
+                      setExpandedGroups([...expandedGroups, group.id]);
+                    }
                   }}
                 />
               </foreignObject>
